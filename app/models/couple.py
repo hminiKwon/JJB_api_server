89f66924076eab09fac26b1268a4daba57f86c9e
hminikwon <jao256@naver.com>
2025-08-07 10:50:23 +0900

    feat: user table, couple table 수정

--- app/models/couple.py
+++ app/models/couple.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+import uuid
+from sqlalchemy import Column, DateTime, Integer, String, func
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 
@@ -6,8 +7,11 @@ class Couple(Base):
     __tablename__ = "couple"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement="auto")
+    room_pwd = Column(String(255), index=True, default=lambda: str(uuid.uuid4()), nullable=False)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
     user = relationship("User", back_populates="user")
 
-    def __repr__(self):
-        return f"<Couple(id={self.id})>"
+    # def __repr__(self):
+    #     return f"<Couple(id={self.id})>"
--- app/models/user.py
+++ app/models/user.py
@@ -1,17 +1,20 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import relationship
 from app.core.database import Base
 
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement="auto")
-    user_id = Column(String(255), index=True)
-    user_name = Column(String(255), index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement="auto", nullable=False)
+    user_id = Column(String(255), index=True, nullable=False)
+    user_pwd = Column(String(255), index=True, nullable=False)
+    user_name = Column(String(255), index=True, nullable=False)
     couple_id = Column(Integer, ForeignKey("couple.id"))
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
 
     couple = relationship("Couple", back_populates="couple")
 
-    def __repr__(self):
-        return f"<User(id={self.id}, user_id='{self.user_id}', user_name='{self.user_name}', couple_id={self.couple_id})>"
+    # def __repr__(self):
+    #     return f"<User(id={self.id}, user_id='{self.user_id}', user_name='{self.user_name}', couple_id={self.couple_id})>"
 
